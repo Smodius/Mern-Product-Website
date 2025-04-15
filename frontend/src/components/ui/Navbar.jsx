f8d670b77fb622405c32b4a865c81c1d96e382f6
Smodius <joshuaseballosc@gmail.com>
2025-04-15 10:24:20 +0800

    refactor: clean up Navbar component and restore color mode toggle functionality

--- frontend/src/components/ui/Navbar.jsx
+++ frontend/src/components/ui/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Box, Button, Container, Flex,Text,Link, HStack } from '@chakra-ui/react'
 import { CiSquarePlus } from "react-icons/ci"
+import { IoMoonOutline } from "react-icons/io5"
+import { useColorMode } from '@chakra-ui/react'
+import { MdOutlineWbSunny } from "react-icons/md"
 
 const Navbar = () => {
-// const { colorMode, toggleColorMode } = useColorMode()
+const { colorMode, toggleColorMode } = useColorMode()
   return ( 
     <Container maxW={"1140px"} px="2">
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"} flexDirection={{base:"column", sm:"row"}}>
@@ -18,16 +21,15 @@ const Navbar = () => {
 					<Link to={"/"}>Product Store 🛒</Link>
 				</Text>
 
-                <HStack spacing ={2} alignItems={"center"}>
+                <HStack spacing={2} alignItems={"center"}>
                     <Link to={"/create"}>
                         <Button colorScheme={"teal"} variant={"solid"}>
                             <CiSquarePlus fontSize={20} /> Create Product   
                         </Button> 
-                        {/* <Button onClick={toggleColorMode} colorScheme={"teal"} variant={"solid"}>
-                            <CiSquarePlus fontSize={20} /> 
-                            {colorMode === "light" ? "Dark" : "Light"} Mode 
-                        </Button>  */}
                     </Link>
+                        <Button onClick={toggleColorMode} colorScheme={"teal"} variant={"solid"}>
+                            {colorMode === "light" ? <IoMoonOutline fontSize={20} /> : <MdOutlineWbSunny fontSize={20}/>}   
+                        </Button> 
                 </HStack>
         </Flex>
     </Container>
